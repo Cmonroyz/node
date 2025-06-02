a8c4a3e23772eb6b49bfa3e4b7d54d55977ed892
Cmonroyz <carlosdavid.monroyzapata@emeal.nttdata.com>
2025-06-02 11:30:03 +0200

    Entry point.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
-const { emailTemplate } = require('./js-foundation/template');
+//const { emailTemplate } = require('./js-foundation/template');
+//const { getUserById } = require('./js-foundation/callbacks');
+// require('./js-foundation/destructuring');
+require('./js-foundation/factory');
 
-console.log(emailTemplate);
+// para revisar el template
+// console.log(emailTemplate);
+
+// para revisar el callback
+// const id = 2;
+// getUserById(id, function(err, user) {
+//   if (err) {
+//     console.error('Error fetching user:', err);
+//   } else {
+//     console.log('User found:', user);
+//   }
+// });
