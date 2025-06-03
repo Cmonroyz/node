3d7bf51d93238341d6f27350a7df8bedd5b4cc43
Cmonroyz <carlosdavid.monroyzapata@emeal.nttdata.com>
2025-06-03 10:42:02 +0200

    Incluir el timestamp para los logs del winston.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { buildLogger } = require('./plugins');
 
 const logger = buildLogger('app.js');
 logger.log('Hola, estoy iniciando la aplicación');
+logger.error('Esto es algo malo');
 
 // getPokemonById(3)
 //   .then((pokemon) => console.log({pokemon}))
