aad4919fcc06162210f398e6c861a53744cd9064
Cmonroyz <carlosdavid.monroyzapata@emeal.nttdata.com>
2025-06-04 15:44:48 +0200

    Test: primer ejemplo de test AAA.

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -2,7 +2,13 @@
 
 
 describe('App', () => {
-  test('should be true', () =>{
-    expect(true).toBe(true);
+  test('should be 3', () =>{
+    // 1 arrange
+      const number = 1;
+      const number2 = 2;
+    // 2 act
+      const result = number + number2;
+    // 3 assert
+      expect(result).toBe(3);
   });
 });
